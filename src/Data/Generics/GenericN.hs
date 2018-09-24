c952786abe7e1895361a527ad4d70d98b830525d
Daniel Gorin <jcpetruzza@gmail.com>
2018-09-24 23:19:30 +0000

    Fix GenericsN.Indexed
    The kind of `original` was broken, so it would only
    compute `Param 0

--- src/Data/Generics/GenericN.hs
+++ src/Data/Generics/GenericN.hs
@@ -31,7 +31,7 @@ import GHC.Generics
 import GHC.TypeLits
 import Data.Coerce
 
-data Param (n :: Nat) (original :: k' -> k) (a :: k)
+data Param (n :: Nat) (original :: k' -> k'') (a :: k)
 
 type family Indexed (t :: k) (i :: Nat) :: k where
   Indexed (t a) i = Indexed t (i + 1) (Param i a)
