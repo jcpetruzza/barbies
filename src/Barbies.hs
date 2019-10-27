318f7d38c1a51fcda1671018cc93d821b513ec6c
Daniel Gorin <jcpetruzza@gmail.com>
2019-10-27 23:51:03 +0000

    Improve docs

--- src/Barbies.hs
+++ src/Barbies.hs
@@ -2,9 +2,11 @@
 -- |
 -- Module:  Barbies
 --
--- A common Haskell idiom is to parameterise a datatype by a type @k -> *@,
--- typically a functor or a GADT. These are like outfits of a Barbie,
--- that turn her into a different doll. E.g.
+-- A common Haskell idiom is to parameterise a datatype by a functor or GADT
+-- (or any type with kind @k -> 'Data.Kind.Type'@, a pattern sometimes
+-- called <https://reasonablypolymorphic.com/blog/higher-kinded-data/ HKD>).
+-- This parameter acts like the outfit of a Barbie, turning it into a different
+--  doll. The canonical example would be:
 --
 -- @
 -- data Person f
@@ -12,14 +14,48 @@
 --       { name :: f 'String'
 --       , age  :: f 'Int'
 --       }
---
--- b1 :: Person 'Data.Monoid.Last'       -- Barbie with a monoid structure
--- b2 :: Person ('Data.Functor.Const.Const' a)  -- 'Barbies.Container' Barbie
--- b3 :: Person 'Data.Functor.Identity.Identity'   -- Barbie's new clothes
 -- @
 --
--- This package define the classes to work with these types and easily
--- transform them. They all come with default instances based on
+-- Let's say that we are writing an application where 'Person' data
+-- will be read from a web form, validated, and stored in a database. Some
+-- possibles outfits that we could use along the way are:
+--
+-- @
+-- Person ('Data.Functor.Const.Const' 'String')  -- for the raw input from the web-form,
+-- Person ('Either' 'String') -- for the result of parsing and validating,
+-- Person 'Data.Functor.Identity.Identity'        -- for the actual data,
+-- Person DbColumn        -- To describe how to read / write a 'Person' to the db
+--
+-- data DbColumn a
+--   = DbColumn
+--       { colName :: 'String'
+--       , fromDb  :: DbDataParser a
+--       , toDb    :: a -> DbData
+--       }
+-- @
+--
+-- In such application it is likely that one will have lots of types like
+-- @Person@ so we will like to handle these transformations uniformly,
+-- without boilerplate or repetitions.  This package provides classes to
+-- manipulate these types, using notions that are familiar to haskellers like
+-- 'Functor', 'Applicative' or 'Traversable'. For example, instead writing
+-- an ad-hoc function that checks that all fields have a correct value, like
+--
+-- @
+-- checkPerson :: Person ('Either' 'String') -> 'Either' ['String'] (Person 'Data.Functor.Identity.Identity')
+-- @
+--
+-- we can write only one such function:
+--
+-- @
+-- check :: 'TraversableB' b => b ('Either' 'String') -> 'Either' ['String'] (b 'Data.Functor.Identity.Identity')
+-- check be
+--   = case 'btraverse' ('either' ('const' 'Nothing') ('Just' . 'Daa.Functor.Identity.Identity')) be of
+--       'Just' bi -> 'Right' bi
+--       'Nothing' -> 'Left' ('bfoldMap' ('either' (:[]) ('const' [])) be)
+-- @
+--
+--  Moreover, these classes come with default instances based on
 -- `GHC.Generics.Generic`, so using them is as easy as:
 --
 -- @
@@ -40,7 +76,7 @@
 
 -----------------------------------------------------------------------------
 module Barbies
-  (  -- * Functor interface
+  (  -- * Functor-based interface
 
      -- | Barbie-types are functors. That means that if one is familiar
      --   with standard classes like 'Functor', 'Applicative' or 'Traversable',
@@ -48,59 +84,67 @@ module Barbies
      --   like one would use:
      --
      -- @
-     --   'fmap' f (xs :: [a])@
+     -- 'fmap' f (as :: [a])
      -- @
      --
      --   to apply @f@ uniformly on every @a@ occurring
-     --   in @xs@, one could use the following to turn a 'Either'-outfit
+     --   in @as@, one could use the following to turn a 'Either'-outfit
      --   into 'Maybe'-outfit:
      --
      -- @
-     --   'bmap' ('either' (\\_ -> 'Nothing') 'Just') (p :: Person ('Either' e))@
+     -- 'bmap' ('either' ('const' 'Nothing') 'Just') (p :: Person ('Either' e))
      -- @
      --
      --   In this case, the argument of 'bmap' will have to be applied on all
      --   fields of @p@:
      --
      -- @
-     --   name p :: 'Either' e 'String'
-     --   age p  :: 'Either' e 'Int'
+     -- name p :: 'Either' e 'String'
+     -- age  p :: 'Either' e 'Int'
      -- @
      --
      --   So 'bmap' here demands a polymorphic function of type:
      --
      -- @
-     --   forall a . 'Either' e a -> 'Maybe' a@
+     -- forall a . 'Either' e a -> 'Maybe' a
+     -- @
+     --
+     --   That is why `bmap` has a rank-2 type:
+     --
+     -- @
+     -- 'bmap' :: 'FunctorB' b => (forall a. f a -> g a) -> b f -> b g
      -- @
      --
      --   Polymorphic functions with 'Applicative' effects can be applied
-     --   using 'btraverse' and the effects will be accumulated. Finally,
-     --   'bzipWith' gives us an analogous of 'Control.Applicative.liftA2':
+     --   using 'btraverse' and the effects will be accumulated:
      --
      -- @
-     --   addDefaults :: Person 'Maybe' -> Person 'Data.Functor.Identity' -> Person 'Data.Functor.Identity'
-     --   addDefaults = 'bzipWith' (\\m d -> 'maybe' d 'pure' m)
+     -- 'btraverse' :: ('TraversableB' b, 'Applicative' t) => (forall a. f a -> t (g a)) -> b f -> t (b g)
      -- @
      --
-     --   Notice that here 'bzipWith' has lifted a function of type:
+     --   Finally, some barbie-types (typically records like @Person@) have an
+     --   'Applicative' structure, and allow us to lift pure n-ary functions
+     --   to functions on barbie-types. For example, 'bzipWith' gives us an analogous
+     --   of 'Control.Applicative.liftA2':
      --
      -- @
-     --   forall a. 'Maybe' a -> 'Data.Functor.Identity' a -> 'Data.Functor.Identity' a
+     -- 'bzipWith' :: 'ApplicativeB' b => (forall a. f a -> g a -> h a) -> b f -> b g -> b h
      -- @
      --
-     --   The 'Functor' class captures endo-functors on 'Data.Kind.Type'. 'FunctorB' is
-     --   for functors from indexed-types to 'Data.Kind.Type'. In particular, barbie-types
-     --   are indexed-types too, so a type parameterized by a barbie is a
-     --   (higher-kinded) barbie as well.
+     -- We can use this to combine barbies:
      --
      -- @
-     --   data Catalog b
-     --     = Catalog (b 'Data.Functor.Identity') (b 'Maybe')
-     --     deriving
-     --       ('GHC.Generics.Generic'
-     --       , 'FunctorB', 'TraversableB', 'ApplicativeB', 'ConstraintsB'
-     --       )
+     -- addDefaults :: Person 'Maybe' -> Person 'Data.Functor.Identity' -> Person 'Data.Functor.Identity'
+     -- addDefaults = 'bzipWith' (\\m d -> 'maybe' d 'pure' m)
      -- @
+     --
+     --   Why is there not a @MonadB@ class as well? As everyone knows,
+     --   <https://james-iry.blogspot.com/2009/05/brief-incomplete-and-mostly-wrong.html a monad is just a monoid in the category of endofunctors>,
+     --   which in this case is a problem, since barbie-types are not endofunctors:
+     --   they map indexed-types to types, unlike the 'Functor' class, that
+     --   captures endo-functors on 'Data.Kind.Type'.
+     --
+     --   All these classes are defined in this module:
      module Data.Functor.Barbie
 
      -- * Bi-functor and nested barbies
