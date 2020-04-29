ec6bb30428f3b09d02dc0664b7e3e7c18cbaf8f3
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-04-30 00:56:27 +0200

    Add the `WearTwo` type family

--- src/Barbies/Internal/Wear.hs
+++ src/Barbies/Internal/Wear.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Barbies.Internal.Wear
-  ( Wear, Bare, Covered
+  ( Wear, Bare, Covered, WearTwo
   )
 
 where
@@ -33,11 +33,56 @@ data Covered
 -- B { f1 :: 5, f2 = 'True' } :: B 'Bare' f
 -- @
 type family Wear t f a where
-  Wear Bare    f a = a
-  Wear Covered f a = f a
+  Wear Bare        f a = a
+  Wear Covered     f a = f a
   Wear (Param _ t) f a = Wear t f a
   Wear t       _ _ = TypeError (     'Text "`Wear` should only be used with "
                                ':<>: 'Text "`Bare` or `Covered`."
                                ':$$: 'Text "`" ':<>: 'ShowType t ':<>: 'Text "`"
                                ':<>: 'Text " is not allowed in this context."
                                )
+
+-- | Like the `Wear` family, but with two wrappers @f@ and @g@ instead of one.
+-- This is useful if you have a data-type where @f@ is parametric but @g@ is
+-- not, consider this:
+--
+-- @
+-- data T t f =
+--   T { f1 :: 'Wear'    t f [Bool]
+--     , f2 :: 'Wear'    t f (Sum Int)
+--     , f3 :: 'WearTwo' t f Sum Int
+--     , f4 :: 'WearTwo' t f Max Int
+--     }
+-- @
+--
+-- with @x :: T Covered Option@ we would have
+--
+-- @
+-- f1 x :: IO (Option [Bool])
+-- f2 x :: IO (Option (Sum Int))
+-- f3 x :: IO (Option (Sum Int))
+-- f4 x :: IO (Option (Max Int))
+-- @
+--
+-- and with @y :: T Bare Identity@ we would have
+--
+-- @
+-- f1 y :: Int
+-- f2 y :: Sum Int
+-- f3 y :: Int
+-- f4 y :: Int
+-- @
+--
+-- Note how @(Option (Sum Int))@ (or @Max@) has a nice Semigroup instance that
+-- we can use to merge two (covered) barbies,
+-- while `WearTwo` removes the wrapper for the bare barbie.
+type family WearTwo t f g a where
+  WearTwo Bare        f g a = a
+  WearTwo Covered     f g a = f (g a)
+  WearTwo (Param _ t) f g a = WearTwo t f g a
+  WearTwo t           _ _ _ =
+    TypeError (     'Text "`WearTwo` should only be used with "
+              ':<>: 'Text "`Bare` or `Covered`."
+              ':$$: 'Text "`" ':<>: 'ShowType t ':<>: 'Text "`"
+              ':<>: 'Text " is not allowed in this context."
+              )
